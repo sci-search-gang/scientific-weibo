b2f11ff314e0ddd57611a0536cee07b8b833f0c4
Myles Horton <myles@getlantern.org>
2013-04-27 17:19:47 +0800

    Added arbitrary whitespace

--- js/collater.js
+++ js/collater.js
@@ -1,7 +1,21 @@
 var collater = {
+    regexify : function(str) {
+        var rx = "";
+        for (var i = 0; i < str.length; i++) {
+            var curChar = str.charAt(i);
+            rx += "\\s*";
+            rx += curChar;
+            //rx += "&#12290;";
+            //rx += "\\u12290+";
+            rx += "\\s*";
+        }
+        return rx;
+    },
+    
     collate : function(resultsArray, searchStr) {
         var hits = new Array();
-        var patt = new RegExp(searchStr);
+        var regexified = this.regexify(searchStr);
+        var patt = new RegExp(regexified);
         for (var j = 0; j < resultsArray.length; j++) {
             var results = resultsArray[j];
             for (var i = 0; i < results.length; i++) {
