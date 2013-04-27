c06811055e2c19b50cf20147ea1c2ed4acd97835
Myles Horton <myles@getlantern.org>
2013-04-27 18:16:59 +0800

    Slightly improved

--- js/resulter.js
+++ js/resulter.js
@@ -3,7 +3,7 @@ var resulter = {
         var all = "<div>";
         for (var i = 0; i < results.length; i++) {
             var r = results[i];
-            all += "\n<div class='res'><a href=''>"+r.text+"</href></div>\n";
+            all += "\n<div><a href='http://weibo.com/"+r.user.id+"'>"+r.text+"</href></div><br><br>\n";
         }
         return all;
     }
