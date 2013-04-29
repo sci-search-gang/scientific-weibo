ac47677070cd36653c2a3ac66832b9107ed1ccfa
Lucas Dixon <ldixon@google.com>
2013-04-29 12:34:41 +0800

    revised bookmarklet

---
+++ js/bookmarklet.js
@@ -0,0 +1,37 @@
+var ScientificWeiboSearch = function () {
+  // TODO: show user that we are loading
+  this.to_load = {
+    'https://raw.github.com/sci-search-gang/scientific-weibo/master/js/scientific_weibo.js': 1,
+    'http://code.jquery.com/jquery-1.9.1.js': 1
+  };
+  this.loaded = [];
+};
+
+ScientificWeiboSearch.prototype.startloading = function () {
+  var self = this;
+  var s = document.createElement('style');
+  s.innerHTML = '#results { width:500px;height:400px; border:1px solid black;  background-color:white; box-shadow:10px 10px 10px black; position:fixed; top: 150px; left:150px; z-index:99999 }';
+  document.getElementsByTagName('head')[0].appendChild(s);
+  for(var s in this.to_load) {
+    var e=document.createElement('script');
+    e.setAttribute('src',s);
+    e.onload = function () {
+      self.loaded.push(s);
+      self.to_load[s] = undefined;
+      self.run();
+      console.log("loaded:" + s);
+    }
+    document.getElementsByTagName('head')[0].appendChild(e);
+  }
+};
+
+ScientificWeiboSearch.prototype.run = function () {
+  console.log(Object.keys(this.loaded));
+  if (Object.keys(this.to_load).length == this.loaded.length) {
+    console.log("running!");
+    doScientificWeibo($('.searchInp_form')[0].value);
+  }
+};
+
+window.scientific_weibo_search = new ScientificWeiboSearch();
+window.scientific_weibo_search.startloading();
