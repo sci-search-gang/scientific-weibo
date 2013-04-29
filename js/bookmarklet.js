f37cd7976adcb47678813ec43e5ca5756eee994f
oliverdog <adamscj+neat@gmail.com>
2013-04-29 13:48:52 +0800

    Title to blue

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -10,7 +10,7 @@ var ScientificWeiboSearch = function () {
 ScientificWeiboSearch.prototype.startloading = function () {
   var self = this;
   var s = document.createElement('style');
-  s.innerHTML = '#results { width:500px; border:1px solid black; background-color:white; box-shadow:5px 5px 5px black; position:absolute; top: 150px; left:150px; z-index:99999; } .res { margin-top:2px; border-bottom: 1px solid #eee; }';
+  s.innerHTML = '#results { width:500px; border:1px solid black; background-color:white; box-shadow:5px 5px 5px black; position:absolute; top: 150px; left:150px; z-index:99999; } .sw_results_h1 { font:"Lucida Sans Unicode"; color:#0078b6;} .res { margin-top:2px; border-bottom: 1px solid #eee; }';
   document.getElementsByTagName('head')[0].appendChild(s);
   for(var s in this.to_load) {
     var e=document.createElement('script');
